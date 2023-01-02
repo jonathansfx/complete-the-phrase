f80e943f97749576ed823dbf26d27d219a6c0cd6
jonathansfx <jonathanluo22@gmail.com>
2023-01-02 15:47:31 -0700

    finish

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 // Event Listeners
 document.getElementById("btnA").addEventListener("click", btnAClicked);
+document.getElementById("btnB").addEventListener("click", btnBClicked);
+document.getElementById("btnC").addEventListener("click", btnCClicked);
+document.getElementById("btnD").addEventListener("click", btnDClicked);
+document.getElementById("btnE").addEventListener("click", btnEClicked);
 
 function btnAClicked() {
-// Alerts
-alert = ("Disguise");
+  alert("Disguise");
+}
 
+function btnBClicked() {
+  alert("Dozen");
+}
 
+function btnCClicked() {
+  alert("Never");
+}
+
+function btnDClicked() {
+  alert("No Gain");
+}
+
+function btnEClicked() {
+  alert("When You're Having Fun");
 }
